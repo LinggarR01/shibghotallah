0eb349e2d78dba8edb5ab80fe2261b9664ef5e7f
LinggarR01 <riza.linggar@gmail.com>
2026-06-12 16:46:00 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  images: {
+    unoptimized: true,
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
